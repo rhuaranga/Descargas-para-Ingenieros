439823657fd0728e156ac032b0cdff9ce12e6008
Ricardo Huaranga Arevalo <rhuaranga@hotmail.es>
2013-06-10 17:47:35 -0500

    comit 1
    este es el comit 1

---
+++ Códigos onmouseover y onmouseout.php
@@ -0,0 +1,5 @@
+onmouseout="this.style.color='white'" onmouseover="this.style.color='#FF0054'"
+
+onmouseout="this.style.background='#131925'" onmouseover="this.style.background='#FF0054'"
+
+onmouseout="this.style.borderBottom='3px solid #131925'" onmouseover="this.style.borderBottom='3px solid #FF0054'"
