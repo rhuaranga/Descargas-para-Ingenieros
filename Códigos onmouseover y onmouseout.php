7ec1621b385869e589994345a21560838d3f785a
Ricardo Huaranga Arevalo <rhuaranga@hotmail.es>
2013-06-10 17:51:53 -0500

    Cambio N° 1 del archivo codigos onmouse over
    Cambio N° 1 del archivo codigos onmouse over que estoy haciendo de
    prueba.

--- Códigos onmouseover y onmouseout.php
+++ Códigos onmouseover y onmouseout.php
@@ -1,3 +1,5 @@
+Hola este es el cambio N° 1 del archivo:
+
 onmouseout="this.style.color='white'" onmouseover="this.style.color='#FF0054'"
 
 onmouseout="this.style.background='#131925'" onmouseover="this.style.background='#FF0054'"
